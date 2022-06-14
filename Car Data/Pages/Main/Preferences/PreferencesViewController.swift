0fa4066f4008534d93ed618e57e827227f71a1f8
Itay Gervash <itayg@balink.net>
2022-06-14 20:18:19 +0300

    prepare preferencesVC

---
+++ Car Data/Pages/Main/Preferences/PreferencesViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PreferencesViewController.swift
+//  Car Data
+//
+//  Created by itay gervash on 14/06/2022.
+//
+
+import UIKit
+
+class PreferencesViewController: CDViewController {
+
+    
+
+    override func setupViews() {
+        super.setupViews()
+        
+    }
+
+}
